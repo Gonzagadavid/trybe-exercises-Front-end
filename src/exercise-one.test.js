bc7ccae686e87efe51efc36b5fbe8e7383d20164
henriqueeyer <henriqueeyer@outlook.com>
2020-05-15 03:15:49 -0300

    Add exercise

---
+++ src/exercise-one.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, afterEach } from '@testing-library/react';
+import TicTacToe from './TicTacToe';
+
+afterEach(cleanup);
+
+describe("Configuração inicial do jogo", () => {
+  test('Verificar se foi renderizada nove casas', () => {
+
+  });
+
+  test('Começar com todos os espaços em branco.', () => {
+
+  });
+
+  test("Começar sem a frase 'Fim de jogo'", () => {
+
+  });
+});
