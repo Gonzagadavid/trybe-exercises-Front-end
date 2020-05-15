64650b2a83e60c480716e8c6e4ae7b1f211ce32c
henriqueeyer <henriqueeyer@outlook.com>
2020-05-15 03:20:22 -0300

    add exercise three

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -10,6 +10,17 @@ class TicTacToe extends React.Component {
     };
   }
 
+  // Antes de começar a realizar o exercício,
+  // junte os dois outros exercícios na master ( mergear os pr).
+  // Atualize essa branch com o git merge master.
+
+  // Adicione nesse arquivo a lógica para identificar
+  // quando que o jogo de ve acabar, necessário passar
+  // nós teste do arquivo exercise-3.test.js;
+
+  // Analise bem como que os teste são realizados 
+  // para criar o que se pede.
+
   render() {
     return <GameBoard gameState={this.state.gameBoard} />;
   }
