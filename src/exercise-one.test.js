6311fe1e56f552978c5c8b13d96dcb864226178e
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-05-26 09:50:37 -0300

    Fix import

--- src/exercise-one.test.js
+++ src/exercise-one.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, afterEach } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import TicTacToe from './TicTacToe';
 
 afterEach(cleanup);
