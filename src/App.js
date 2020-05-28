9864a6684c84693fab8dd44050cc0d22ee5c96c2
lauro <laurolyra@gmail.com>
2020-05-28 11:37:21 -0300

    [CONTENT REVIEW] 17.4 Adding exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchForm from './components/SearchForm';
+import CharInfo from './components/CharInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchForm />
+      <CharInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CharInfo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+// importe o connect do 'react-redux';
+
+class CharInfo extends React.Component {
+  render() {
+    //faça a desestruturação das props aqui
+
+    if (!loading && character) {
+      return (
+        <ul>
+          <li>Name: {character.name}</li>
+          <li>Gender: {character.gender}</li>
+          <li>Aliases: {character.aliases.map((alias, index) => <p key={`${alias}-${index}`}>{alias}</p>)}</li>
+          <li>Books: {character.books.map((book, index) => <p key={`${book}-${index}`}>{book}</p>)}</li>
+        </ul>
+      )
+    }
+    if (error) { return <div>{error}</div>; }
+    if (loading) { return <div>Loading...</div>; }
+    return <div>Type a character name and click to search!</div>;
+  }
+};
+
+//mapeie o estado global para a propriedade da sua aplicação
+
+
+// conecte este componente ao redux aqui
+
+
+CharInfo.propTypes = {
+  loading: propTypes.bool.isRequired,
+  character: propTypes.arrayOf(propTypes.object),
+  error: propTypes.string,
+};
+
+CharInfo.defaultProps = {
+  character: null,
+  error: null,
+};
+
