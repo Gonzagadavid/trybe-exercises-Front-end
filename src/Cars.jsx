0c30fa5f531cd4339a1c7f52b0c870b6401edcb5
Gabriel <dreadpool38@gmail.com>
2020-07-01 11:54:19 -0300

    [ADD] Setup exercise two

---
+++ src/Cars.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import carBlue from './images/carBlue.jpeg';
+import carRed from './images/carRed.jpeg';
+import carYellow from './images/carYellow.jpeg';
+
+function Cars({
+  redCar, blueCar, yellowCar, moveCar,
+}) {
+  return (
+    <div>
+      <div>
+        <img className={redCar ? 'car-right' : 'car-left'} src={carRed} alt="red car" />
+        <button onClick={() => moveCar('red', !redCar)} type="button">move</button>
+      </div>
+      <div>
+        <img className={blueCar ? 'car-right' : 'car-left'} src={carBlue} alt="blue car" />
+        <button onClick={() => moveCar('blue', !blueCar)} type="button">move</button>
+      </div>
+      <div>
+        <img className={yellowCar ? 'car-right' : 'car-left'} src={carYellow} alt="yellow car" />
+        <button onClick={() => moveCar('yellow', !yellowCar)} type="button">move</button>
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  redCar: /* coloque seu código aqui  */
+  blueCar: /* coloque seu código aqui */
+  yellowCar: /* coloque seu código aqui */
+});
+
+const mapDispatchToProps = () => ({ /* coloque seu código aqui  */  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cars);
