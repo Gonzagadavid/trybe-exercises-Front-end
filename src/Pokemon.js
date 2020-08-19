1e4aa11afd872191015ebd6c9fd77fe9cf10d327
Douglas Henrique <doughrqe@gmail.com>
2020-08-19 10:56:47 -0300

    typo fix

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -4,14 +4,13 @@ import './pokemon.css';
 class Pokemon extends React.Component {
   render() {
     const {name, type, averageWeight, image} = this.props.pokemon;
-
     return (
       <div className="pokemon">
         <div>
           <p>{name}</p>
           <p>{type}</p>
           <p>
-            Average weight: `${averageWeight.value} ${averageWeight.measurementUnit}`
+            Average weight: {`${averageWeight.value} ${averageWeight.measurementUnit}`}
           </p>
         </div>
         <img src={image} alt={`${name} sprite`} />
