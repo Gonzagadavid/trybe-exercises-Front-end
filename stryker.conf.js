5d00fa24b892e120fa726f6586909b1765159b7d
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-10-08 11:24:27 -0300

    Fixing

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,32 +2,29 @@
  * @type {import('@stryker-mutator/api/core').StrykerOptions}
  */
 module.exports = {
-  $schema: "./node_modules/@stryker-mutator/core/schema/stryker-schema.json",
   _comment:
     "This config was generated using a preset. Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react",
-  //testRunner: "jest",
-  testRunner: "command",
+  testRunner: "jest",
+  mutate: [
+    "src/App.js"
+  ],
   commandRunner: {
-    command: "CI=true npm test 'src/tests/App.test.js'"
+    "command": "CI=true npm test 'src/App.test.js'"
   },
-  mutate: ['src/App.js?(x)', 'src/Digimon.js?(x)'],
+  // excludedMutations: [
+  //   "ArithmeticOperator",
+  //   "ArrayDeclaration",
+  //   "ArrowFunction",
+  //   "BlockStatement",
+  //   "BooleanLiteral",
+  //   "ConditionalExpression",
+  //   "EqualityOperator",
+  //   "LogicalOperator",
+  //   "ObjectLiteral",
+  //   "UnaryOperator",
+  //   "UpdateOperator"
+  // ],
   reporters: ["progress", "clear-text", "html"],
-  mutator: {
-    name: "javascript",
-    excludedMutations: [
-      "ArithmeticOperator",
-      "ArrayDeclaration",
-      "ArrowFunction",
-      "BlockStatement",
-      "BooleanLiteral",
-      "ConditionalExpression",
-      "EqualityOperator",
-      "LogicalOperator",
-      "ObjectLiteral",
-      "UnaryOperator",
-      "UpdateOperator"
-    ]
-  },
   coverageAnalysis: "off",
   jest: {
     projectType: "create-react-app",
