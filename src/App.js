999ab217be321df08640f70249762f769036bbcf
rfreitasbatista <rfreitasbatista@gmail.com>
2020-11-03 10:24:06 -0300

    Correções exercícios 2.2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Greeting({initialName = ''}) {
+function Greeting({ initialName = '' }) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(initialName);
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
 
   function handleChange(event) {
-    setName(event.target.value)
+    setName(event.target.value);
   }
   return (
     <div>
@@ -20,11 +20,11 @@ function Greeting({initialName = ''}) {
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting />;
 }
 
-export default App
+export default App;
