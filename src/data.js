460bcbcd9da254dc4863c34f8ac27a2654b5f95a
EduardoSantosF <eduardo.debest81@gmail.com>
2021-04-29 16:18:44 -0300

    update poke images url

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const pokemons = [
           value: "6.0",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png",
+      image: "https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)"
   },
   {
@@ -18,7 +18,7 @@ const pokemons = [
           value: "8.5",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/0/0a/Spr_5b_004.png",
+      image: "https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)"
   },
   {
@@ -29,7 +29,7 @@ const pokemons = [
           value: "2.9",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/8/83/Spr_5b_010.png",
+      image: "https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)"
   },
   {
@@ -40,7 +40,7 @@ const pokemons = [
           value: "6.9",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/1/18/Spr_5b_023.png",
+      image: "https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)"
   },
   {
@@ -51,7 +51,7 @@ const pokemons = [
           value: "48.0",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png",
+      image: "https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)"
   },
   {
@@ -62,7 +62,7 @@ const pokemons = [
           value: "4.0",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/4/43/Spr_5b_151.png",
+      image: "https://cdn2.bulbagarden.net/upload/4/43/Spr_5b_151.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)"
   },
   {
@@ -73,7 +73,7 @@ const pokemons = [
           value: "95.0",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/5/58/Spr_5b_078.png",
+      image: "https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)"
   },
   {
@@ -84,7 +84,7 @@ const pokemons = [
           value: "460.0",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/4/40/Spr_5b_143.png",
+      image: "https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Snorlax_(Pok%C3%A9mon)"
   },
   {
@@ -95,7 +95,7 @@ const pokemons = [
           value: "16.5",
           measurementUnit: "kg"
       },
-      image: "https://cdn.bulbagarden.net/upload/2/2c/Spr_5b_148.png",
+      image: "https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png",
       moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)"
   }
 ];
