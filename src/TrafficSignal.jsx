e355d0bfd1eab66ef7b4b624f7f374d48f697d33
Gabriel Almeida <56565362+gabriellukke@users.noreply.github.com>
2021-05-26 20:16:35 -0300

    Revert "Finalizado exercicio 3"

--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import { changeSignal } from './redux/actionCreators'
 import redSignal from './images/redSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
@@ -18,25 +16,15 @@ const renderSignal = (signalColor) => {
   return null;
 };
 
-function TrafficSignal({ signalColor, changeSignalColor }) {
+export default function TrafficSignal({ signalColor, changeSignal }) {
   return (
     <div>
       <div className="button-container">
-        <button onClick={() => changeSignalColor('red')} type="button">Red</button>
-        <button onClick={() => changeSignalColor('yellow')} type="button">Yellow</button>
-        <button onClick={() => changeSignalColor('green')} type="button">Green</button>
+        <button onClick={() => changeSignal('red')} type="button">Red</button>
+        <button onClick={() => changeSignal('yellow')} type="button">Yellow</button>
+        <button onClick={() => changeSignal('green')} type="button">Green</button>
       </div>
       <img className="signal" src={renderSignal(signalColor)} alt="" />
     </div>
   );
 }
-
-const mapStateToProps = (state) => ({
-  signalColor: state.changeSinal.signal.color,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  changeSignalColor: (color) => dispatch(changeSignal(color))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(TrafficSignal);
