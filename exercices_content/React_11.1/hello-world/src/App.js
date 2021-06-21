e34d68e24c021cdb9ef36369b7e162b286328914
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-21 16:02:32 -0300

    estudado e feito exercicios do conteudo 11.1

---
+++ exercices_content/React_11.1/hello-world/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Sobre from './Component';
+
+function App() {
+  return (< Sobre />);
+}
+
+export default App;
---
+++ exercices_content/React_11.1/hello-world/src/Component.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+      <h1>David Gonzaga</h1>
+      <p>Por que não usar a extensão jsx?</p>
+      </div>
+    );
+  };
+};
+
+export default Sobre
