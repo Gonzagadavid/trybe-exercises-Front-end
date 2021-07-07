e6f900ea1a9cea123f05181e9b7d234f82385de5
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-07-07 18:25:06 -0300

    feito exercicios bonus

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import './pokemon.css';
 import { Link} from 'react-router-dom'
+import {AiFillStar} from 'react-icons/ai'
 
 
 class Pokemon extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      favorite: false,
+    }
+    this.renderFavorites = this.renderFavorites.bind(this)
+  }
+
+  componentDidMount() {
+    this.renderFavorites()
+  }
+  
+  componentDidUpdate() {
+    this.renderFavorites()   
+  }
+
+  renderFavorites() {
+    const { pokemon} = this.props;
+    const { id } = pokemon;
+    const pokedexJson = localStorage.getItem('pokedex') || "[]"
+    const pokedex = JSON.parse(pokedexJson)
+    const bool = pokedex.includes(`${id}`)
+    if (this.state.favorite !== bool) {
+      this.setState({favorite: bool})
+    }
+  }
+
   render() {
     const {name, type, averageWeight, image, id} = this.props.pokemon;
+    const { favorite } = this.state
+    const styleObj = {color: favorite ? 'goldenrod' : 'transparent', fontSize: '25px'}
     return (
       <div className="pokemon">
         <div>
+        <p>{<AiFillStar style={styleObj}/>}</p>
           <p>{name}</p>
           <p>{type}</p>
           <p>
