4bf26387cec2b652f7ad1cfc679c8c0b2b40ae7f
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-07-17 19:27:14 -0300

    feito merge na branch do exercise-one

--- src/test/exercise-one.test.js
+++ src/test/exercise-one.test.js
@@ -4,7 +4,6 @@ import App from '../App';
 
 afterEach(cleanup);
 
-// arquivo App.test.js pode servir de exemplo
 describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
   test('Verificando se o botão está na tela e se o ele contém o texto "Adicionar"', () => {
     const {getByText} = render(<App />);
@@ -13,7 +12,6 @@ describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
   });
 
   test(`Ao clicar no botão, é necessário adicionar o que o usuário digitou à lista`, () => {
-    // Use os fireEvent, para simular a digitação do usuário e o clique.
     const {getByLabelText,queryByText, getByTestId} = render(<App />);
     const inputTodo = getByLabelText('Tarefa:');
     const addBtn = queryByText('Adicionar');
