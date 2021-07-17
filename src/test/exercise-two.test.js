c36c9f597635d42aef651b070accc4a19d2aae8e
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-07-17 20:13:29 -0300

    desenvolvido teste para que ao adicionar uma sequencia de tarefas as mesmas sao renderizadas na tela e testado se ao passar um texto como props para o component Item o mesmo é renderizado na tela

--- src/test/exercise-two.test.js
+++ src/test/exercise-two.test.js
@@ -8,13 +8,23 @@ afterEach(cleanup);
 describe('Teste do campo de input', () => {
   test('Testando a adição de vários itens a aplicação', () => {
     const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água']; // Use esse array como base para realizar os testes.
-    const {} = render(<App />) // Caso precise de uma nova query adicione no object destructuring
-
+    const {getByLabelText, queryByText, getByTestId } = render(<App />) // Caso precise de uma nova query adicione no object destructuring
+    const inputTodo = getByLabelText('Tarefa:');
+    const addBtn = queryByText('Adicionar');
+    const todoList = getByTestId('todo-list');
+    
+    listTodo.forEach((task) => {
+      fireEvent.change(inputTodo, { target: { value: task}});
+      fireEvent.click(addBtn);
+      expect(todoList).toHaveTextContent(task);
+    })
   })
 });
 
 describe('Teste do componente Item', () => {
   test('Ao receber uma string como prop ela precisa aparecer na tela', () => {
-
+    const { getByText } = render(<Item  content="test task"/>)
+    const itemTest = getByText('test task')
+    expect(itemTest).toBeInTheDocument();
   })
 })
