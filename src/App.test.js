effd3b0724a6413e14ed937e4f421ae5afefea77
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-07-20 00:25:02 -0300

    desenvolvidos testes de funcionalidade e renderizacao do component App

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitForDomChange } from '@testing-library/react';
 import App from './App';
+
+const resp = [{"name":"Angemon","img":"https://digimon.shadowsmith.com/img/angemon.jpg","level":"Champion"}]
 
 describe('Teste da aplicação toda', () => {
 
-  it('renders App', async () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/Faça uma pesquisa/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+  global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve(resp)
+  }))
 
+  it('renders App', async () => {
+    const { getByText, getByTestId } = render(<App />);
+    const linkElement = getByText(/Faça uma pesquisa/i);
+     expect(linkElement).toBeInTheDocument();
+    
+      const input = getByTestId('input');
+      fireEvent.change(input, { target : { value: 'angemon'}})
+      expect(input.value).toBe('angemon')
+
+      const button = getByTestId('buttonSearch');
+      expect(button).toBeInTheDocument();
+      fireEvent.click(button)
+      await waitForDomChange()
+      const h2 = getByText('Angemon')
+      expect(h2).toBeInTheDocument()
+      expect(global.fetch).toHaveBeenCalled();
+      expect(linkElement).not.toBeInTheDocument();
+    });
 });
--- src/Digimon.test.js
+++ src/Digimon.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Digimon from './Digimon';
 
+const digimon = {"name":"Angemon","img":"https://digimon.shadowsmith.com/img/angemon.jpg","level":"Champion"}
+
+
 describe('Teste da tela do Digimon', () => {
   it('renders Digimon', async () => {
-
+    const {} = render(<Digimon digimon={digimon} />)
   });
 });
