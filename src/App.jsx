09bd60fdb44177643363dfdbbdc1dbf26a4e7f15
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-07-20 22:58:16 -0300

    adicionado rotas e o arquivo renderWithRouter para auxiliar no testes

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom'
+import './App.css';
+import Home from './pages/Home'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import NotFound from './pages/NotFound'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>David Gonzaga</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Home</Link>
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/projects" component={Projects} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div>Essa Página não existe</div>
+    );
+  };
+}
+
+export default NotFound;
