8f79c6660bc073d51721025a7972e67cdadea10c
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-07-23 19:50:29 -0300

    estudado e adicionado estudos do conteudo 16.1

---
+++ exercises_content/Redux_16.1/createRedux.js
@@ -0,0 +1,26 @@
+import Redux from 'redux'
+
+const fazerLogin = (email) => ({ type: "LOGIN", email});
+
+const ESTADO_INICIAL = { login: false, email: ""};
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+    default: // No switch, sempre precisamos ter um caso default!
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+store.dispatch(fazerLogin("alguem@email.com"));
+
+console.log(store.getState());
+
+// { login: true, email: 'alguem@email.com' }
