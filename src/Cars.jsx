a841ea513db2f94e9b4e3ec2837755276688cf35
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-08-16 20:34:23 -0300

    refatorado componentes para de class para funcionais usando hooks

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,59 +1,56 @@
 // src/Cars.jsx
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-class Cars extends Component {
-  render() {
-    const { redCar, blueCar, yellowCar } = this.context.cars;
-    const { moveCar } = this.context;
-    return (
+const Cars = () => {
+  const { redCar, blueCar, yellowCar, moveCar } = useContext(CarsContext);
+  return (
+    <div>
       <div>
-        <div>
-          <img
-            className={redCar ? 'car-right' : 'car-left'}
-            src={carRed}
-            alt="red car"
-          />
-          <button
-            onClick={() => moveCar('redCar', !redCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={blueCar ? 'car-right' : 'car-left'}
-            src={carBlue}
-            alt="blue car"
-          />
-          <button
-            onClick={() => moveCar('blueCar', !blueCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
-        <div>
-          <img
-            className={yellowCar ? 'car-right' : 'car-left'}
-            src={carYellow}
-            alt="yellow car"
-          />
-          <button
-            onClick={() => moveCar('yellowCar', !yellowCar)}
-            type="button"
-          >
-            Move
-        </button>
-        </div>
+        <img
+          className={redCar ? 'car-right' : 'car-left'}
+          src={carRed}
+          alt="red car"
+        />
+        <button
+          onClick={() => moveCar('redCar', !redCar)}
+          type="button"
+        >
+          Move
+      </button>
       </div>
-    )
-  }
+      <div>
+        <img
+          className={blueCar ? 'car-right' : 'car-left'}
+          src={carBlue}
+          alt="blue car"
+        />
+        <button
+          onClick={() => moveCar('blueCar', !blueCar)}
+          type="button"
+        >
+          Move
+      </button>
+      </div>
+      <div>
+        <img
+          className={yellowCar ? 'car-right' : 'car-left'}
+          src={carYellow}
+          alt="yellow car"
+        />
+        <button
+          onClick={() => moveCar('yellowCar', !yellowCar)}
+          type="button"
+        >
+          Move
+      </button>
+      </div>
+    </div>
+  )
 };
 
 Cars.contextType = CarsContext;
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,45 +1,24 @@
 // src/context/Provider.js
 
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CarsContext from './CarsContext';
 
-class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: {
-        red: false,
-        blue: false,
-        yellow: false,
-      },
-    }
-    this.moveCar = this.moveCar.bind(this);
-  }
-
-  moveCar(car, side) {
-    this.setState({
-      cars: {
-        ...this.state.cars,
-        [car]: side,
-      },
-    });
+const Provider = ({ children }) => {
+  const INITIAL_STATE =  { red: false, blue: false, yellow: false }
+  const [cars, setCars] = useState(INITIAL_STATE)
+ 
+  const moveCar = (car, side) => {
+    setCars({ ...cars, [car]: side,});
   };
 
-  render() {
-    const context = {
-      ...this.state,
-      moveCar: this.moveCar,
-    };
+  const context = { ...cars, moveCar };
 
-    const { children } = this.props;
-
-    return (
-      <CarsContext.Provider value={context}>
-        {children}
-      </CarsContext.Provider>
-    );
-  }
+  return (
+    <CarsContext.Provider value={context}>
+      {children}
+    </CarsContext.Provider>
+  );
 };
 
 Provider.propTypes = {
