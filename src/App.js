7a36f24f6ada48b48da81e51c136feb286c5192e
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-08-19 00:20:17 -0300

    desenvolvido codigo para iniciar com o nome salvo no localStorage e salvar ao ser digitado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Greeting({ initialName = '' }) {
   // 🐨 inicialize o estado com o valor que vem do localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName);
+  const saveName = localStorage.getItem('name') || initialName
+  const [name, setName] = useState(saveName);
 
   // 🐨 Utilize o hook useEffect para atualizar a
   // propriedade `name` no localStorage quando o estado for alterado
   // 💰 window.localStorage.setItem('name', name)
+  
+  useEffect(() => {
+    localStorage.setItem('name', name)
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value);
@@ -16,7 +21,7 @@ function Greeting({ initialName = '' }) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
