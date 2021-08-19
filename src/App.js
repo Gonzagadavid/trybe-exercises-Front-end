7fe2ea2c9dcf6c020a3ac03dc322becf7824752e
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-08-18 23:53:24 -0300

    conectado campo do input com o estado utilizando hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 function Greeting() {
   // substitua essa variável por uma variável no estado, utilizando useState
-  const name = '';
+  const [ name, setName] = useState('');
 
-  function handleChange(event) {
     // atualize o valor do estado com base no que está em event.target.value
-  }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={({ target: { value } }) => setName(value)} value={name}id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
